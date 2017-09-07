ab18c7d31a13494de6877f7c1f8840d46b3692b8
Patrick Cho <cppcho.hk@gmail.com>
2017-09-07 13:59:38 +0800

    multi

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@i
+M=0 // i = 0
+@R2
+M=0 // R2 = 0
+
+(LOOP)
+@i
+D=M   // D = i
+@R1
+D=D-M // D = i - R1
+
+@LOOPEND
+D;JGE // Jump to LOOPEND if i - R1 >= 0
+
+@R0
+D=M   // D = R0
+@R2
+M=D+M   // R2 = R0 + R2
+@i
+M=M+1   // i = i + 1
+
+@LOOP
+0;JMP // Jump to LOOP
+(LOOPEND)
+
+(END)
+@END
+0;JMP
