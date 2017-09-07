91f7cf556a1f001b5543caf4cfc3f10ead81861d
Patrick Cho <cppcho.hk@gmail.com>
2017-09-07 14:46:47 +0800

    fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,65 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+(LOOP)  // infinite loop
+
+// Keyboard
+
+@KBD
+D=M   // Current key pressed, 0 if empty
+
+@BLACK
+D;JNE // Jump if any key is being pressed
+D=0
+@BLACKEND
+0;JMP
+(BLACK)
+D=-1
+(BLACKEND)
+@pixel
+M=D
+
+// Set SCREEN
+
+@i
+M=0   // i = 0
+
+(DRAWSCREEN)
+
+// 8192 * 16bits
+@i
+D=M
+@8192
+D=D-A
+@ENDDRAWSCREEN
+D;JGE   // End if i < 8192
+
+@SCREEN
+D=A
+@i
+D=D+M
+@screenpos
+M=D
+@pixel
+D=M
+@screenpos
+A=M
+M=D
+
+@i
+M=M+1   // i = i + 1
+
+@DRAWSCREEN
+0;JMP
+(ENDDRAWSCREEN)
+
+@LOOP
+0;JMP // Jump to LOOP (infinite loop)
